b945e48d4369e01ec8b8666bd305f033d31b4079
yash.rh <yash.rh@somaiya.edu>
2020-10-03 20:32:19 +0530

    Updates

---
+++ precache-manifest.1bb3be7cf50999a83e79e11e1484fd63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd440e8d6aa4494b6199e47ddbc29e02",
+    "url": "/RecipeAPI/index.html"
+  },
+  {
+    "revision": "8f11ad0c1f83f72b690e",
+    "url": "/RecipeAPI/static/css/main.9ab7833a.chunk.css"
+  },
+  {
+    "revision": "02d10e00ec3bf7d88a8b",
+    "url": "/RecipeAPI/static/js/2.8ce2e91f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeAPI/static/js/2.8ce2e91f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f11ad0c1f83f72b690e",
+    "url": "/RecipeAPI/static/js/main.a99c04b9.chunk.js"
+  },
+  {
+    "revision": "37775697235c8676f3ec",
+    "url": "/RecipeAPI/static/js/runtime-main.f230f106.js"
+  }
+]);
